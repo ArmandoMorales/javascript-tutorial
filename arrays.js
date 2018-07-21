025cebbc1c977df59a189d9c26a2e7ec03316fea
Armando Morales <armandomorales2004@gmail.com>
2018-07-21 16:41:49 -0500

    seccion de arrays y arreglos
    contiene:
    declarar un array.
    acceder a un elemento.
    numero de elementos de un array.
    recorrer un array.

---
+++ arrays.js
@@ -0,0 +1,18 @@
+"use strict"
+
+// Formas de declarar un array
+var saludo = [ "Hola", "Mundo" ];
+var lenguajes = new Array( "php", "js", "go" );
+
+// Acceder a un elemento deacuerdo a la posicion
+console.log( "saludo[0]:", saludo[0] );
+console.log( "lenguajes[2]:", lenguajes[2] );
+
+// Numero de elementos de un array
+console.log( "saludo.length:", saludo.length );
+console.log( "lenguajes.length:", lenguajes.length );
+
+// Recorrer un array ( index y arreglo son opcionales )
+lenguajes.forEach( function( elemento, index, arreglo ) {
+   console.log( elemento, index, arreglo );
+});
